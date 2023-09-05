a50738be3f07b2387bd304a2a1740a54864cb03c
Vadims Malickis <117085106+VadimsMalickis@users.noreply.github.com>
2023-09-05 15:55:31 +0000

    Init

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
     }
 }
