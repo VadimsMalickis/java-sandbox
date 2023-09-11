0b490314d8a1d9750718f9b2f7688f3f3e0fa30c
VadimsMalickis <vadims.malickis@rvt.lv>
2023-09-11 19:20:36 +0300

    test

--- src/App.java
+++ src/App.java
@@ -6,9 +6,14 @@ public class App {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("What is your name?");
-        
         String name = scanner.nextLine();
 
-        System.out.println("Hello " + name);
+        System.out.println("What is your age?");
+        int age = scanner.nextInt();
+
+        System.out.println("Hello " + name + " you are " + age + " years old!");
+
+        scanner.close();
     }
+
 }
