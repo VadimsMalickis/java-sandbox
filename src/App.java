7e3f3e28fda9efba150176ce4ef90d19e23a9c70
VadimsMalickis <vadims.malickis@rvt.lv>
2023-09-19 18:50:00 +0300

    simple console programm

--- src/App.java
+++ src/App.java
@@ -1,19 +1,47 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-
+    public static void main(String[] args) throws Exception { // main method
+        
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> travelItemList = new ArrayList<String>();
+        boolean programIsRunning = true;
 
-        System.out.println("What is your name?");
-        String name = scanner.nextLine();
+        while (programIsRunning == true) {
+            System.out.println("Choose action: add, remove or show");
+    
+            String command = scanner.nextLine().toLowerCase().trim();
+    
+            switch (command) {
+                case "add":
+                    System.out.println("Type item name to add");
+                    travelItemList.add(scanner.nextLine());
+                    break;
+    
+                case "remove":
+                    System.out.println("Type item name to remove");
+                    String itemToRemove = scanner.nextLine();
+                     for (String item : travelItemList) {
+                        if (item.equals(itemToRemove)) {
+                            travelItemList.remove(itemToRemove);
+                        }
+                    }
+                    break;
+                case "show":
+                    System.out.println("Total item: " + travelItemList.size());
+                    for (String item : travelItemList) {
+                        System.out.println(item);
+                    }
+                    break;
+                case "exit":
+                    System.out.println("Exit program....");
+                    programIsRunning = false;
+            }
+        }
 
-        System.out.println("What is your age?");
-        int age = scanner.nextInt();
-
-        System.out.println("Hello " + name + " you are " + age + " years old!");
-
-        scanner.close();
     }
+        
+        
 
 }
