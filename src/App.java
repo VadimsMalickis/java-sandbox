deb4a0a4716ffb1723e48e00fa7a68f7ff0d60b2
VadimsMalickis <vadims.malickis@rvt.lv>
2023-10-28 10:37:59 +0300

    some changes

--- src/App.java
+++ src/App.java
@@ -1,17 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
         ArrayList<Student> studentList = StudentCsvManager.getAllStudents();
 
         for (Student student : studentList) {
             System.out.println(student.getName());
         }
 
+        Sorter sorter = new Sorter();
+
+        Scanner scanner = new Scanner(System.in);
+
+        sorter.drawTriangle();
 
     }
-        
 
 }
